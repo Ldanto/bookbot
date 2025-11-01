009cb22839840088b975f5650b37d2a58b1bdcb6
Ldanto <lolcanogod@gmail.com>
2025-11-01 17:53:11 -0500

    Added word count function and stats.py. Moved word count function to stats.py. Now main.py prints word count

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
+from stats import get_word_count
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
-    
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_word_count(text)
+    print(f"Found {num_words} total words")
+
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+#def get_word_count(file_contents):
+ #   words_in_file = file_contents.split()
+ #   return len(words_in_file)
+
+
+   
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(file_contents):
+    words_in_file = file_contents.split()
+    return len(words_in_file)
